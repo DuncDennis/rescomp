1a2f6c48ea4d9a19ea297a7982f605275b2d90e4
Duncan, Dennis <DuncDennis@gmail.com>
2022-03-20 14:40:16 +0100

    Improved hdf5 viewer app and added more plotting funcs

--- rescomp/plotting.py
+++ rescomp/plotting.py
@@ -1,19 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import rescomp.measures as measures
 
 
-def plot_attractor(out):
-    N_ens = out.shape[0]
-    nr_of_time_intervals = out.shape[1]
+#### Some functions to plot a trajectory results numpy array:
+def plot_attractor(out, N_ens=None, nr_of_time_intervals=None):
+    print(out.shape)
+    if N_ens is None:
+        N_ens = out.shape[0]
+    if nr_of_time_intervals is None:
+        nr_of_time_intervals = out.shape[1]
     y_pred_all = out[:, :, 0, :, :]
     y_test_all = out[:, :, 1, :, :]
+
+    # N_ens = 2
+    # nr_of_time_intervals = 2
+
     fig, axs = plt.subplots(N_ens, nr_of_time_intervals, figsize=(5*nr_of_time_intervals, 5*N_ens))
 
-    # for i_ens in range(N_ens):
-    #     for i_interval in range(nr_of_time_intervals):
-
-    for i_ens in range(2):
-        for i_interval in range(2):
+    for i_ens in range(N_ens):
+        for i_interval in range(nr_of_time_intervals):
             ax = axs[i_ens, i_interval]
             y_pred = y_pred_all[i_ens, i_interval, :, :]
             y_test = y_test_all[i_ens, i_interval, :, :]
@@ -21,5 +27,110 @@ def plot_attractor(out):
             ax.plot(y_pred[:, 0], y_pred[:, 2], linewidth=1, label="pred")
             ax.set_title(f"ENS: {i_ens}, Timeinterval: {i_interval}")
             ax.legend()
+    return fig
 
+
+def plot_difference(trajs, params_to_show, f):
+    fig = plt.figure()
+    ax = plt.gca()
+
+    for i_traj, traj in enumerate(trajs):
+        data = f["runs"][traj][:]
+        diff = data[:, :, 0, :, :] - data[:, :, 1, :, :]
+        mean_diff = np.mean(diff, axis=(0, 1))
+        plt.plot(mean_diff[:, 0], label=f"{params_to_show[i_traj]}")
+    plt.legend()
+    return fig
+
+def plot_error(trajs, params_to_show, f, max_x):
+    fig = plt.figure()
+
+    for i_traj, traj in enumerate(trajs):
+        data = f["runs"][traj][:]
+        error = get_error(data)
+        mean_error = np.mean(error, axis=(0, 1))
+        plt.plot(mean_error[:max_x], label=f"{params_to_show[i_traj]}")
+    plt.legend()
+    return fig
+
+
+def plot_attractor_2(trajs, params_to_show, f, i_ens, i_time_period, base_fig_size=(5, 10)):
+    fig, axs = plt.subplots(len(trajs), 1, figsize=(base_fig_size[0], base_fig_size[1]*len(trajs)))
+
+    for i_traj, traj in enumerate(trajs):
+        ax = axs[i_traj]
+        data = f["runs"][traj][:][i_ens, i_time_period, :, :, :]
+        y_pred = data[0, :, :]
+        y_test = data[1, :, :]
+        ax.plot(y_test[:, 0], y_test[:, 2], linewidth=1, label="test")
+        ax.plot(y_pred[:, 0], y_pred[:, 2], linewidth=1, label="true")
+        ax.set_title(f"{params_to_show[i_traj]}")
+        ax.legend()
+    return fig
+
+
+def plot_trajectories(trajs, params_to_show, f, i_ens, i_time_period, base_fig_size=(5, 10)):
+    fig, axs = plt.subplots(len(trajs), 1, figsize=(base_fig_size[0], base_fig_size[1]*len(trajs)))
+
+    for i_traj, traj in enumerate(trajs):
+        ax = axs[i_traj]
+        data = f["runs"][traj][:][i_ens, i_time_period, :, :, :]
+        y_pred = data[0, :, :]
+        y_test = data[1, :, :]
+        ax.plot(y_test[:, 0], label="test")
+        ax.plot(y_pred[:, 0], label="true")
+        ax.set_title(f"{params_to_show[i_traj]}")
+        ax.legend()
+    return fig
+
+
+# Utility:
+def get_error(data):
+    n_ens = data.shape[0]
+    n_interval = data.shape[1]
+    n_pred_steps = data.shape[3]
+
+    error = np.zeros((n_ens, n_interval, n_pred_steps))
+    for i_ens in range(n_ens):
+        for i_interval in range(n_interval):
+            y_pred = data[i_ens, i_interval, 0, :, :]
+            y_test = data[i_ens, i_interval, 1, :, :]
+
+            error[i_ens, i_interval, :] = measures.error_over_time(y_pred, y_test, distance_measure="L2",
+                                                                    normalization="root_of_avg_of_spacedist_squared")
+    return error
+
+
+def plot_valid_times_heatmap(trajs, params_to_show, f, error_threshhold, base_fig_size=(5, 10)):
+    fig, axs = plt.subplots(1, len(trajs), figsize=(base_fig_size[0]*len(trajs), base_fig_size[1]))
+    # v_min = None
+    # v_max = None
+    for i_traj, traj in enumerate(trajs):
+        try:
+            ax = axs[i_traj]
+        except:
+            ax = axs
+        data = f["runs"][traj][:]
+        n_ens, n_interval = data.shape[0], data.shape[1]
+        error = get_error(data)
+
+        valid_times = np.zeros((n_ens, n_interval))
+        for i_ens in range(n_ens):
+            for i_interval in range(n_interval):
+                valid_times[i_ens, i_interval] = measures.valid_time_index(error[i_ens, i_interval, :],
+                                                                           error_threshhold)
+
+        # if v_min is None:
+        #     v_min = np.min(valid_times)
+        # else:
+        #     v_min_new = np.min(valid_times)
+        #     if v_min < v_min_new:
+        #         v_min = v_min_new
+
+        im = ax.imshow(valid_times)
+        ax.set_xlabel("i_time_period")
+        ax.set_ylabel("i_ens")
+        ax.set_title(f"{params_to_show[i_traj]}")
+
+    # fig.colorbar(im)
     return fig
--- streamlit_apps/hdf5_viewer.py
+++ streamlit_apps/hdf5_viewer.py
@@ -3,6 +3,9 @@ import pathlib
 import h5py
 import os
 import matplotlib.pyplot as plt
+plt.style.use('dark_background')
+import numpy as np
+import pandas as df
 import rescomp.plotting as plot
 import rescomp.statistical_tests as stattest
 
@@ -13,8 +16,43 @@ def load_hdf5_file(file_path):
     return f
 
 
-def check_parameter_sweep(f):
-    pass
+def check_parameter_sweep(f, stripped=True):
+    params_dict = {}
+    for trajectory in list(f["runs"].keys()):
+        for key, val in f["runs"][trajectory].attrs.items():
+            if key in params_dict.keys():
+                if stripped:
+                    if val not in params_dict[key]:
+                        params_dict[key].append(val)
+                else:
+                    params_dict[key].append(val)
+            else:
+                params_dict[key] = [val, ]
+    return params_dict
+
+
+def get_trajectories(multiselect_dict, f):
+    for key, val in multiselect_dict.items():
+        if len(val) == 0:
+            st.warning(f"Choose at least one option for {key}")
+            raise Exception(f"Choose at least one option for {key}")
+
+    trajs = []
+    params_to_show = []
+    for trajectory in list(f["runs"].keys()):
+        select_traj = True
+        for key, val in f["runs"][trajectory].attrs.items():
+            if val not in multiselect_dict[key]:
+                select_traj = False
+                break
+        if select_traj:
+            params_this_traj = {}
+            trajs.append(trajectory)
+            for key, val in multiselect_dict.items():
+                if len(val)>1:
+                    params_this_traj[key] = f["runs"][trajectory].attrs[key]
+            params_to_show.append(params_this_traj)
+    return trajs, params_to_show
 
 
 repo_path = pathlib.Path(__file__).parent.resolve().parents[0]
@@ -28,29 +66,49 @@ option = st.sidebar.selectbox(
 
 if option is not None:
     f = load_hdf5_file(pathlib.Path.joinpath(path, option))
-    # st.sidebar.write('You selected:', f"{f.keys()}")
 
-    runs = list(f["runs"].keys())
+    params_dict = check_parameter_sweep(f)
 
-    option2 = st.sidebar.selectbox(
-        'runs', runs)
+    multiselect_dict = {}
+    for key, val in params_dict.items():
+        multiselect_dict[key] = st.sidebar.multiselect(key, val)
 
-    for key, val in f["runs"][option2].attrs.items():
-        st.sidebar.write(f"{key}: {val}")
+    # st.write(multiselect_dict)
+    trajs, params_to_show = get_trajectories(multiselect_dict, f)
 
-    data = f["runs"][option2][:]
-    st.write(data.shape)
+    with st.container():
+        plot_vt = st.checkbox("plot valid times heatmap")
+        if plot_vt:
+            error_threshhold = st.slider("error threshhold", min_value=0.00001, max_value=3., step=0.001, value=0.5)
+            fig = plot.plot_valid_times_heatmap(trajs, params_to_show, f, error_threshhold=error_threshhold)
+            st.pyplot(fig)
 
+    with st.container():
+        plot_error = st.checkbox("plot error")
+        if plot_error:
+            max_x = st.slider("max_x", min_value=5, max_value=1000, step=1, value=500)
+            fig = plot.plot_error(trajs, params_to_show, f, max_x)
+            st.pyplot(fig)
 
-    # fig, ax = plt.subplots()
-    # ax.plot(data[0, 0, 0, :, 0], data[0, 0, 0, :, 2], linewidth=1, label="pred")
-    # ax.plot(data[0, 0, 1, :, 0], data[0, 0, 1, :, 2], linewidth=1, label="true")
-    # ax.set_xlim(-20, 20)
-    # ax.set_ylim(5, 45)
-    # plt.legend()
-    # ax.plot(y_pred[:, 0], y_pred[:, 2], linewidth=1, label="pred")
+    with st.container():
+        plot_difference = st.checkbox("plot difference")
+        if plot_difference:
+            fig = plot.plot_difference(trajs, params_to_show, f)
+            st.pyplot(fig)
 
-    # fig = plot.plot_attractor(data)
-    # st.pyplot(fig)
+    plot_attractor = st.checkbox("plot attractor")
+    plot_trajectory = st.checkbox("plot trajectory")
+    if plot_attractor or plot_trajectory:
+        N_ens = f["runs"][trajs[0]][:].shape[0]
+        i_ens = st.number_input("i_ens", min_value=0, max_value=N_ens-1)
+        N_time_periods = f["runs"][trajs[0]][:].shape[1]
+        i_time_period = st.number_input("i_time_period", min_value=0, max_value=N_time_periods-1)
 
-# st.button("test")
+        if plot_attractor:
+            fig = plot.plot_attractor_2(trajs, params_to_show, f, i_ens, i_time_period, base_fig_size=(15, 4))
+            st.pyplot(fig)
+
+        if plot_trajectory:
+            fig = plot.plot_trajectories(trajs, params_to_show, f, i_ens, i_time_period, base_fig_size=(15, 4))
+            st.pyplot(fig)
+
