7a3c4b19b3851cca6ae1857c61b0a898754a7a50
Duncan, Dennis <DuncDennis@gmail.com>
2022-08-30 13:40:54 +0200

    Add new experimental measures "largest_cross_lyapunov_exponent".

--- rescomp/measures_new.py
+++ rescomp/measures_new.py
@@ -264,3 +264,74 @@ def extrema_map(time_series: np.ndarray, mode: str = "minima", i_dim: int = 0) -
 
     extreme = x[ix]
     return np.array([extreme[:-1], extreme[1:]]).T
+
+
+def largest_cross_lyapunov_exponent(
+    iterator_func: Callable[[np.ndarray], np.ndarray],
+    predicted_trajectory: np.ndarray,
+    deviation_scale: float = 1e-10,
+    steps: int = int(1e3),
+    part_time_steps: int = 15,
+    steps_skip: int = 50,
+    dt: float = 1.0,
+    initial_pert_direction: np.ndarray | None = None,
+    return_convergence: bool = False,
+) -> float | np.ndarray:
+    """Numerically calculate a cross lyapunov exponent to measure the quality of the prediction.
+
+    The output is a lyapunov-exponent-like measure that measures the exponential deviation of the
+    real system (given by the iterator_func) from a predicted time series.
+
+    The algorithm is very similar to "largest_lyapunov_exponent", but instead of using a
+    real-system reference orbit, it uses the predicted trajectory as the reference orbit.
+
+    Args:
+        iterator_func: Function to iterate the real system to the next time step: x(i+1) = F(x(i)).
+        predicted_trajectory: The predicted trajectory.
+        deviation_scale: The L2-norm of the initial perturbation.
+        steps: Number of renormalization steps.
+        part_time_steps: Time steps between renormalization steps.
+        steps_skip: Number of renormalization steps to perform, before tracking the log divergence.
+                Avoid transients by using steps_skip.
+        dt: Size of time step.
+        initial_pert_direction:
+            - If np.ndarray: The direction of the initial perturbation.
+            - If None: The direction of the initial perturbation is assumed to be np.ones(..).
+        return_convergence: If True, return the convergence of the largest LE; a numpy array of
+                            the shape (N, ).
+
+    Returns:
+        The largest cross Lyapunov Exponent. If return_convergence is True: The convergence
+        (np.ndarray), else just the float value, which is the last value in the convergence.
+    """
+
+    predicted_time_steps, x_dim = predicted_trajectory.shape
+
+    if predicted_time_steps < (steps_skip + steps) * part_time_steps:
+        raise Exception("Predicted trajectory is not long enough for the specified steps. ")
+
+    if initial_pert_direction is None:
+        initial_pert_direction = np.ones(x_dim)
+
+    initial_perturbation = initial_pert_direction * (deviation_scale / np.linalg.norm(initial_pert_direction))
+
+    log_divergence = np.zeros(steps)
+
+    x = predicted_trajectory[0, :]
+    x_pert = x + initial_perturbation
+
+    for i_n in range(steps + steps_skip):
+        for i_t in range(part_time_steps):
+            # x = iterator_func(x)
+            x_pert = iterator_func(x_pert)
+        x = predicted_trajectory[(i_n + 1) * part_time_steps, :]
+        dx = x_pert - x
+        norm_dx = np.linalg.norm(dx)
+        x_pert = x + dx * (deviation_scale / norm_dx)
+        if i_n >= steps_skip:
+            log_divergence[i_n - steps_skip] = np.log(norm_dx / deviation_scale)
+
+    if return_convergence:
+        return np.array(np.cumsum(log_divergence) / (np.arange(1, steps + 1) * dt * part_time_steps))
+    else:
+        return float(np.average(log_divergence) / (dt * part_time_steps))
